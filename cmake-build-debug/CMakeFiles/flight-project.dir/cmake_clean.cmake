19e44d3624c4def43859d0479ee4d32c5af23b1b
hodyah <hodyahadler@gmail.com>
2019-12-19 16:55:40 +0200

    18.12 lexer: מפרק את הקובץ פקודות למחסנית על הheap . יש הדגמה  ב main לכיצד הlexer מפרק את הפקודות.
     19.12 server:
     לא גמור - צריך לבדוק שבאמת ככה עושים, וגם הTREAD לא עובד טוב, משהו שם לא נכון. צריך לחזור לזה

---
+++ cmake-build-debug/CMakeFiles/flight-project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flight-project.dir/lexer.cpp.o"
+  "CMakeFiles/flight-project.dir/main.cpp.o"
+  "CMakeFiles/flight-project.dir/server.cpp.o"
+  "flight-project"
+  "flight-project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flight-project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
